20dc2f217272e9d39e9ffd9b6067de9f757617c0
advaittinaikar <advait.tinaikar>
2016-11-18 18:03:06 -0500

    Initial Commit-Skills3

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source: 'http://www.rubygems.org'
+
+gem 'sinatra'
+gem 'shotgun'
+gem 'activesupport'
+
+gem "rake"
+gem 'activerecord'
+gem 'sinatra-activerecord' # excellent gem that ports ActiveRecord for Sinatra
+
+gem 'json'
+gem 'haml'
+gem 'builder'
+
+gem 'twilio-ruby'
+
+gem 'dotenv'
+
+group :development do
+	gem 'pg'
+	
+end
---
+++ app.rb
@@ -0,0 +1,72 @@
+require 'sinatra'
+require 'json'
+require 'activerecord'
+require 'active_support/all'
+require 'active_support/ext'
+require 'twilio-ruby'
+require 'sqlite3'
+
+configure :development do
+	require 'pg'
+end
+
+enable :sessions
+
+configure :development do
+	require 'dotenv'
+	Dotenv.load
+end
+
+client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+
+get '/' do
+	"Basic Application working"
+	#401
+end
+
+get '/from' do
+	"This is from #{ENV['TWILIO_FROM']}"
+end
+
+get '/send_sms' do
+	client.account.Messages.create {
+		:from =>ENV["TWILIO_FROM"],
+		:to=>"+16462580532",
+		:body=>"You've just asked to send a message to yourself!"
+	}
+	"Sent message!"
+end
+
+get '/incoming_sms' do
+	session['counter']||=0
+	count=session['count']
+
+	sender=params[:From]||=''
+	#receiver=params[:To]||=''
+	body=params[:Body]||=''
+
+	body=body.downcase.strip
+
+	if count < 1
+		message="So #{sender}, this is your first message. Woohoo!"
+	else
+		message="You said #{body} from your phone number #{sender}"
+
+	# client.account.Messages.create {
+	# 	:from =>ENV["TWILIO_FROM"],
+	# 	:to=>sender,
+	# 	:body=>message
+	# }
+
+	twiml= Twilio::TwiML::Response.new do |t|
+		t.Message message	
+	end
+
+	content_type 'text/xml'
+
+	twiml.text
+end
+
+error '401' do
+	"Hola you made a mistake. But mistakes happen!"
+end
